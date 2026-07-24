d216db68172bdea8f3c68400ed5b8e78888fb4ea
SrikethanReddy <srikethanreddyganji@gmail.com>
2026-07-24 07:51:04 +0530

    Initial project setup

---
+++ ai-database-analyst/src/main/java/com/example/ai_database_analyst/AiDatabaseAnalystApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_database_analyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDatabaseAnalystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDatabaseAnalystApplication.class, args);
+	}
+
+}
---
+++ ai-database-analyst/src/test/java/com/example/ai_database_analyst/AiDatabaseAnalystApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_database_analyst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDatabaseAnalystApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
